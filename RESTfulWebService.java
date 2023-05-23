c6e1f2c92b0771fcc196186fbe44b9f9e4e4e1e8
ahmedhamdyy166 <109494742+ahmedhamdyy166@users.noreply.github.com>
2023-05-23 18:07:49 +0300

    Implementation for the api path

---
+++ RESTfulWebService.java
@@ -0,0 +1,8 @@
+package toolsProject;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class RESTfulWebService extends Application {}
+
